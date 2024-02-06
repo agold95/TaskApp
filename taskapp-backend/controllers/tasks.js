7ea844eb880fbf294c3d5d794b9c0974789917b2
Adam Goldberg <112718049+agold95@users.noreply.github.com>
2024-02-06 14:49:14 -0500

    added more backend tests

--- taskapp-backend/controllers/tasks.js
+++ taskapp-backend/controllers/tasks.js
@@ -1,6 +1,9 @@
 const tasksRouter = require('express').Router()
 const Task = require('../models/task')
 
+// helper to check if deadline is formatted correctly
+const isValidDate = (dateString) => !isNaN(Date.parse(dateString))
+
 // get all tasks
 tasksRouter.get('/', async (request, response) => {
   const user = request.user
@@ -21,8 +24,29 @@ tasksRouter.post('/', async (request, response) => {
 
   const user = request.user
 
+  // returns if user is not signed in
   if (!user) {
     return response.status(401).json({ error: 'Operation not permitted' }).end()
+  }
+
+  // returns if task content is empty
+  if (!task.content) {
+    return response.status(400).json({ error: 'Task content cannot be empty' }).end()
+  }
+
+  // returns if deadline is malformatted
+  if (deadline && !isValidDate(deadline)) {
+    return response.status(400).json({ error: 'Malformatted deadline' })
+  }
+
+  // returns if content is greater than 3 characters long
+  if (task.content.length < 3) {
+      return response.status(400).json({ error: 'Task content must be greater than 3 characters' }).end()
+  }
+
+  // returns if task content is longer than 100 characters long
+  if (task.content.length > 100) {
+      return response.status(400).json({ error: 'Task content must be less than 100 characters' }).end()
   }
 
   task.user = user._id
@@ -45,6 +69,31 @@ tasksRouter.put('/:id', async (request, response) => {
   const task = {
     content: body.content,
     deadline: body.deadline
+  }
+
+  // returns if task content is empty
+  if (!task.content) {
+    return response.status(400).json({ error: 'Task content cannot be empty' }).end()
+  }
+
+  // returns if task content is greater than 3 characters long
+  if (task.content.length < 3) {
+      return response.status(400).json({ error: 'Task content must be greater than 3 characters' }).end()
+  }
+
+  // returns if deadline is malformatted
+  if (task.deadline && !isValidDate(task.deadline)) {
+    return response.status(400).json({ error: 'Malformatted deadline' })
+  }
+
+  // returns if content is greater than 3 characters long
+  if (task.content.length < 3) {
+      return response.status(400).json({ error: 'Task content must be greater than 3 characters' }).end()
+  }
+
+  // returns if task content is longer than 100 characters long
+  if (task.content.length > 100) {
+      return response.status(400).json({ error: 'Task content must be less than 100 characters' }).end()
   }
 
   const updatedTask = await Task.findByIdAndUpdate(id, task, { new: true })
--- taskapp-backend/controllers/users.js
+++ taskapp-backend/controllers/users.js
@@ -7,7 +7,7 @@ usersRouter.post('/', async (request, response) => {
   const { username, password, passwordConfirmation } = request.body
 
   // checks for username/password length
-  if (password.length <= 3 || username.length <= 3) {
+  if (password.length < 3 || username.length < 3) {
       return response.status(400).json({ error: 'Username or password must be longer than 3 characters' }).end()
   }
 
